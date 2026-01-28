beaa7a930c8664508e8dc082898715abccd156ce
Sahil Ali <sahilali9bmdss@gmail.com>
2026-01-28 16:44:38 +0545

    first update

--- calculator.py
+++ calculator.py
@@ -1,1 +1,23 @@
-a= 10
+first_num = float(input("Enter first number: "))
+second_num = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+if operation == '+':
+    result = first_num + second_num
+    print("Result:", result)
+elif operation == '-':
+    result = first_num - second_num
+    print("Result:", result)
+elif operation == '*':
+    result = first_num * second_num
+    print("Result:", result)
+elif operation == '/':
+    if second_num != 0:
+        result = first_num / second_num
+        print("Result:", result)
+    else:
+        result = "Error: Division by zero is not allowed."
+        print(result)
+else:
+    result = "Error: Invalid operation"
+    print(result)
+
